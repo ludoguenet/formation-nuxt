e040ea15a5f81e53d68bfc5677293618b5c65555
ludoguenet <ludovicguenet@gmx.com>
2024-04-20 16:31:20 +0200

    added NuxtLinks

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <nav class="navigation">
+      <NuxtLink to="/">Homepage</NuxtLink>
+      <NuxtLink to="/characters">Characters</NuxtLink>
+      <NuxtLink to="https://laraveljutsu.net">Laravel Jutsu</NuxtLink>
+    </nav>
     <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+.navigation {
+  display: flex;
+  gap: 5px;
+}
+</style>
